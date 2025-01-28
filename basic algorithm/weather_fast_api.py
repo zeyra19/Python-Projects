fb383a729d353626966e0520790b1408ef8d5385
zeyra19 <zehranurhindioglu@gmail.com>
2025-01-28 23:50:41 +0300

    Create weather app import and install

--- basic algorithm/weather_fast_api.py
+++ basic algorithm/weather_fast_api.py
@@ -1,5 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+# Jinja HTML dosyalarını dinamik olarak oluşturmaya olanak tanır.
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 url = ""
+
+cities = {}
